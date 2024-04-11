f21dee0e48aac75d61149f8b360da6ef2803fb9c
Sajuna94 <122368633+Sajuna94@users.noreply.github.com>
2024-04-11 16:28:14 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -6,8 +6,17 @@
 
 typedef enum { EMPTY = -1, BLACK = 0, WHITE = 1 } Square;
 
+typedef struct {
+    int x;
+    int y;
+    Square player;
+} Coordinate;
+
 typedef struct
 {
+    Coordinate* cord;
+    int cordSize;
+
     Square board[BOARD_SIZE][BOARD_SIZE];
 } Chess;
 
@@ -25,8 +34,9 @@ int calculateWeight(Chess* chess, int x, int y);
 
 // CHESS
 void displayChess(Chess* chess);
-void newChess();
-bool autoSetMove(Chess* chess);
+Chess* newChess();
+Point getNextMove(Chess* chess);
+bool setXY(Chess* chess, int x, int y, Square player);
 
 // ==================================================================================================================================================
 // CHESS
@@ -46,27 +56,38 @@ void displayChess(Chess* chess) {
     }
 }
 
-void newChess() {
-    Chess *chess = (chess*)malloc(sizeof(Chess));
+Chess* newChess() {
+    Chess *chess = (Chess*)malloc(sizeof(Chess));
 
     for (int y = 0; y < BOARD_SIZE; y++)
         for (int x = 0; x < BOARD_SIZE; x++)
         {
-            chess.board[y][x] = EMPTY;
+            chess->board[y][x] = EMPTY;
         }
 
     return chess;
 }
 
-// 自動根據權重下棋 (沒寫完)
-bool autoSetMove(Chess* chess, Square stoneColor) {
+Point getNextMove(Chess* chess) {
     int length;
     Point *weightPoints = getWeightPoints(chess, &length);
 
-    Point targetPoint = weightPoints[length - 1];
+    return weightPoints[length - 1];
+}
 
+bool setXY(Chess* chess, int x, int y, Square player) {
+    if (chess->board[y][x] != EMPTY)
+        return false;
 
-    chess->board[targetPoint.Y][targetPoint.X] = stoneColor;
+    // update cord
+    chess->cord = (Coordinate*)realloc(chess->cord, (chess->cordSize + 1) * sizeof(Coordinate));
+    chess->cord[chess->cordSize].x = x;
+    chess->cord[chess->cordSize].y = y;
+    chess->cord[chess->cordSize].player = player;
+    chess->cordSize++;
+
+    // update board
+    chess->board[y][x] = player;
 
     return true;
 }
@@ -77,9 +98,7 @@ bool autoSetMove(Chess* chess, Square stoneColor) {
 
 // 計算權重
 int calculateWeight(Chess* chess, int x, int y) {
-    if (chess->board[y][x] != EMPTY)
-        return 0;
-    return 333;
+    return rand() % 100; // 0 ~ 99
 }
 
 int comparePoint(const void* a, const void* b) {
@@ -115,18 +134,25 @@ Point* getWeightPoints(Chess* chess, int* length) {
 }
 
 void displayWeight(Point *points, int length) {
-    int weightMap[BOARD_SIZE][BOARD_SIZE];
+    int weightMap[BOARD_SIZE][BOARD_SIZE] = { 0 };
+    Point point = points[length - 1];
 
     for (int i = 0; i < length; i++) {
         weightMap[points[i].Y][points[i].X] = points[i].value;
     }
     
+    for (int i = 0; i < BOARD_SIZE * 7; i++) printf("="); printf("\n");
+
     for (int y = 0; y < BOARD_SIZE; y++) {
         for (int x = 0; x < BOARD_SIZE; x++) {
-            printf("%d ", weightMap[y][x]);
+            printf("%4d | ", weightMap[y][x]);
         }
         printf("\n");
     }
+
+    for (int i = 0; i < BOARD_SIZE * 7; i++) printf("="); printf("\n");
+    printf("Max Point(%d, %d), Value: %d\n", point.X, point.Y, point.value);
+    for (int i = 0; i < BOARD_SIZE * 7; i++) printf("="); printf("\n");
 }
 
 // ==================================================================================================================================================
@@ -134,13 +160,18 @@ void displayWeight(Point *points, int length) {
 // ==================================================================================================================================================
 
 int main(void) {
-    Chess chess = newChess();
+    Chess *chess = newChess();
+
+    for (int i = 0; i < BOARD_SIZE; i++)
+        chess->board[2][i] = BLACK;
+    for (int i = 0; i < BOARD_SIZE; i++)
+        chess->board[i][i] = WHITE;
 
     int length = 0;
-    Point *weightPoints = getWeightPoints(&chess, &length);
+    Point *weightPoints = getWeightPoints(chess, &length);
 
     displayWeight(weightPoints, length);
-    displayChess(&chess);
+    displayChess(chess);
 
     free(weightPoints);
     free(chess);
